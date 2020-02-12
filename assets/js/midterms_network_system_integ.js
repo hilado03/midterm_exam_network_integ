6578f5289a260ecdb7b4d8e60d8fb79c29150aca
Patrick Edrianne Calagos <edriannecalagos@gmail.com>
2020-02-12 18:56:10 +0800

    edited files

--- assets/js/midterms_network_system_integ.js
+++ assets/js/midterms_network_system_integ.js
@@ -1,4 +1,4 @@
-/** mga id ng select
+                                                                                                                                                                                                                                                                                                                                                                                                      /** mga id ng select
     #mask_slash
     #mask_ip
     #number_of_hosts
@@ -8,18 +8,18 @@ $(document).ready(function(){
     $("#mask_slash").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_ip").prop('selectedIndex', pipiliin);
-        $("number_of_hosts").prop('selectedIndex', pipiliin);
+        $("#number_of_hosts").prop('selectedIndex', pipiliin);
     });
 });
 $(document).ready(function(){
     $("#mask_ip").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_slash").prop('selectedIndex', pipiliin);
-        $("number_of_hosts").prop('selectedIndex', pipiliin);
+        $("#number_of_hosts").prop('selectedIndex', pipiliin);
     });
 });
 $(document).ready(function(){
-    $("number_of_hosts").change(function(){
+    $("#number_of_hosts").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_ip").prop('selectedIndex', pipiliin);
         $("#mask_slash").prop('selectedIndex', pipiliin);
