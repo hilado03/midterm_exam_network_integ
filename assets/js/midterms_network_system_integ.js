232d715a28d1ab51239e7733241c0855addfd180
hermoso <hermosoross@gmail.com>
2020-02-12 18:42:20 +0800

    edited files

---
+++ assets/js/midterms_network_system_integ.js
@@ -0,0 +1,27 @@
+/** mga id ng select
+    #mask_slash
+    #mask_ip
+    #number_of_hosts
+*/
+
+$(document).ready(function(){
+    $("#mask_slash").change(function(){
+        var pipiliin = $(this).prop('selectedIndex');
+        $("#mask_ip").prop('selectedIndex', pipiliin);
+        $("number_of_hosts").prop('selectedIndex', pipiliin);
+    });
+});
+$(document).ready(function(){
+    $("#mask_ip").change(function(){
+        var pipiliin = $(this).prop('selectedIndex');
+        $("#mask_slash").prop('selectedIndex', pipiliin);
+        $("number_of_hosts").prop('selectedIndex', pipiliin);
+    });
+});
+$(document).ready(function(){
+    $("number_of_hosts").change(function(){
+        var pipiliin = $(this).prop('selectedIndex');
+        $("#mask_ip").prop('selectedIndex', pipiliin);
+        $("#mask_slash").prop('selectedIndex', pipiliin);
+    });
+});
