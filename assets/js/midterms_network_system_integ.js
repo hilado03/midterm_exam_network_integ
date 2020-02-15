2f88781904937a20dd4bf5feceee1cb79225b0d7
Patrick Edrianne Calagos <edriannecalagos@gmail.com>
2020-02-15 11:16:03 +0800

    edited files

--- assets/js/midterms_network_system_integ.js
+++ assets/js/midterms_network_system_integ.js
@@ -4,71 +4,24 @@
     #number_of_hosts
 */
 
-
-/*
 $(document).ready(function(){
     $("#mask_slash").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_ip").prop('selectedIndex', pipiliin);
-        $("#number_of_hosts").prop('selectedIndex', pipiliin);
+        $("#num_clients").prop('selectedIndex', pipiliin);
     });
 });
 $(document).ready(function(){
     $("#mask_ip").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_slash").prop('selectedIndex', pipiliin);
-        $("#number_of_hosts").prop('selectedIndex', pipiliin);
+        $("#num_clients").prop('selectedIndex', pipiliin);
     });
 });
 $(document).ready(function(){
-    $("#number_of_hosts").change(function(){
+    $("#num_clients").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_ip").prop('selectedIndex', pipiliin);
         $("#mask_slash").prop('selectedIndex', pipiliin);
-    });
-});
-
-*/
-
-$(document).ready(function(){
-$("#mask_slash").change(function(){
-    var idx = $(this).prop('selectedIndex');
-    $("#mask_ip").prop('selectedIndex', idx);
-
-    var val_slash = $(this).val();
-    var val_clients = 0;
-
-    switch (val_slash){
-        case 21:
-            val_clients = "2046";
-            break;
-        case 22:
-            val_clients = "1022";
-            break;
-        case 23:
-            val_clients = "510";
-            break;
-        case 24:
-            val_clients = "254";
-            break;
-        case 25:
-            val_clients = "126";
-            break;
-        case 26:
-            val_clients = "62";
-            break;
-        case 27:
-            val_clients = "30";
-            break;
-        case 28:
-            val_clients = "14";
-            break;
-        case 29:
-            val_clients = "6";
-            break;
-        case 30:
-            val_clients = "2";
-            break;
-    }
     });
 });
