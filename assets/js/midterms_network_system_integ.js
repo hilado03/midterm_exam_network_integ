d94809cca7e00521359cd98f345cdaa23b51208b
Patrick Edrianne Calagos <edriannecalagos@gmail.com>
2020-02-15 11:28:27 +0800

    edited files

--- assets/js/midterms_network_system_integ.js
+++ assets/js/midterms_network_system_integ.js
@@ -2,6 +2,7 @@
     #mask_slash
     #mask_ip
     #num_clients
+    #bit_mask
 */
 
 $(document).ready(function(){
@@ -9,19 +10,33 @@ $(document).ready(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_ip").prop('selectedIndex', pipiliin);
         $("#num_clients").prop('selectedIndex', pipiliin);
+        $("#bit_mask").prop('selectedIndex', pipiliin);
     });
 });
+
 $(document).ready(function(){
     $("#mask_ip").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_slash").prop('selectedIndex', pipiliin);
         $("#num_clients").prop('selectedIndex', pipiliin);
+        $("#bit_mask").prop('selectedIndex', pipiliin);
     });
 });
+
 $(document).ready(function(){
     $("#num_clients").change(function(){
         var pipiliin = $(this).prop('selectedIndex');
         $("#mask_ip").prop('selectedIndex', pipiliin);
         $("#mask_slash").prop('selectedIndex', pipiliin);
+        $("#bit_mask").prop('selectedIndex', pipiliin);
+    });
+});
+
+$(document).ready(function(){
+    $("#bit_mask").change(function(){
+        var pipiliin = $(this).prop('selectedIndex');
+        $("#mask_ip").prop('selectedIndex', pipiliin);
+        $("#mask_slash").prop('selectedIndex', pipiliin);
+        $("#num_clients").prop('selectedIndex', pipiliin);
     });
 });
